38ce1549be45fe22c99af2febcaac03f937962be
bashyahub <Sebastian.Kmiotek1@marist.edu>
2025-04-02 12:52:20 -0400

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -2,11 +2,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the client directory
+// Log current directory
+console.log('Current directory:', __dirname);
+
+// Serve static files from the correct location
 app.use(express.static(path.join(__dirname, '../client/public')));
 
-// Route for the main HTML page
+console.log('Static files directory:', path.join(__dirname, '../client/public'));
+
+// Serve index.html for the root route
 app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/views/index.html'));
+});
+
+// Serve index.html for any other routes
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '../client/views/index.html'));
 });
 

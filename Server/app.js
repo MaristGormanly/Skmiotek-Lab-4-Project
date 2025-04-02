6239eb165fbfc33ede478d5a908c7831744ba0b7
bashyahub <Sebastian.Kmiotek1@marist.edu>
2025-04-02 14:39:13 -0400

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the client directory
+app.use(express.static(path.join(__dirname, '../client/public')));
 
-// Example route
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// Route for the main HTML page
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/views/index.html'));
+});
 
 // Start the server
-app.listen(1337, () => console.log('Skull Kap Studios running on http://localhost:1337'));
-});
+app.listen(1337, () => console.log('Skull Kap Studios listening on port 1337!'));

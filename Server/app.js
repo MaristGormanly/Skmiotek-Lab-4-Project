681c1b69bc5b8676c171cd2587dfcea1ac07161a
bashyahub <Sebastian.Kmiotek1@marist.edu>
2025-04-21 23:25:20 -0400

    Update app.js
    struggling

--- Server/app.js
+++ Server/app.js
@@ -1,26 +1,31 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 const bodyParser = require('body-parser');
-const websiteFeatureRoutes = require('../route/websiteFeatureRoute');
-const websiteFeatureApp = express(); 
+const app = express();
 
-websiteFeatureApp.use(bodyParser.json());
-
+app.use(bodyParser.json()); 
 // serve static files from the client directory
-app.use(express.static(path.join(__dirname, '../client/public')));
+app.use(express.static(path.join(__dirname, '../client/public'))); 
 
 // route for the main HTML page
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../client/views/index.html'));
 });
 
-// route for userRoute.js
+// route for userRoute API
 let userRoutes = require('../route/userRoute');
 app.use('/api/user', userRoutes);
 
-// websitefeature API routes
-websiteFeatureApp.use('/api/websiteFeature', websiteFeatureRoutes);
+// route for website feature API
+let websiteFeatureRoutes = require('../route/websiteFeatureRoute');
+app.use('/api/websiteFeature', websiteFeatureRoutes);
 
-// start server
-app.listen(1337, () => console.log('Skull Kap Studios listening on port 1337!'));
+// console log for errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+// declare port and start server
+const PORT = process.env.PORT || 1337;
+app.listen(PORT, () => console.log(`Skull Kap Studios listening on port ${PORT}!`));
